34ec4c12571f6b5b0ff0c4b7f7f695cd743c8774
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2025-04-13 23:14:34 +0530

    banner

--- src/Component/addBanner/addBanner.jsx
+++ src/Component/addBanner/addBanner.jsx
@@ -36,7 +36,7 @@ const AddBanner = () => {
         formData.append('image', image);
 
         try {
-            const response = await axios.post('https://clothing-backend-one.vercel.app/user/add-banner', formData, {
+            const response = await axios.post('https://stitch-commerce-admaya.vercel.app/user/upload', formData, {
                 headers: { 'Content-Type': 'multipart/form-data' },
             });
             console.log('Banner added:', response.data);
